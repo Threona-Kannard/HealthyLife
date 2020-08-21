48a5a2271388e6e02205440ae70fc6fc47db01af
Huỳnh Tấn Phát <66297538+Threona-Kannard@users.noreply.github.com>
2020-08-21 08:03:55 +0700

    nộp deadline 1 (#2)
    Co-authored-by: ldhuy2306 <67452098+ldhuy2306@users.noreply.github.com>

--- app/src/main/java/threona/kannard/healthylife/WaterActivity.kt
+++ app/src/main/java/threona/kannard/healthylife/WaterActivity.kt
@@ -1,15 +1,31 @@
 package threona.kannard.healthylife
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
 class WaterActivity : AppCompatActivity() {
 
+    private var total: Float = 0f;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_water)
 
+
+
+        val btn_water_icon = findViewById<Button>(R.id.add_water)
+        btn_water_icon?.setOnClickListener()
+        {
+            if ()
+            if (total<2){
+            val total_water = findViewById<TextView>(R.id.Water_count)
+            total = total+0.25f
+            total_water.setText(total.toString())}
+        }
     }
 }
