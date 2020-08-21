5209c442a96e5eb75e4c33d17c03692083434f09
Huỳnh Tấn Phát <66297538+Threona-Kannard@users.noreply.github.com>
2020-08-21 21:48:22 +0700

    Update Firestore

--- app/src/main/java/threona/kannard/healthylife/SignUpActivity.kt
+++ app/src/main/java/threona/kannard/healthylife/SignUpActivity.kt
@@ -1,10 +1,9 @@
 package threona.kannard.healthylife
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.CheckBox
+import androidx.appcompat.app.AppCompatActivity
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
