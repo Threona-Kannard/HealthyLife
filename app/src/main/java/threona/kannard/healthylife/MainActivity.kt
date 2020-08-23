34e8d1319f93eb85a74851a593f3b3eec37c4ca2
Huỳnh Tấn Phát <66297538+Threona-Kannard@users.noreply.github.com>
2020-08-23 21:47:58 +0700

    Final Commit Midterm Project

--- app/src/main/java/threona/kannard/healthylife/MainActivity.kt
+++ app/src/main/java/threona/kannard/healthylife/MainActivity.kt
@@ -1,14 +1,36 @@
 package threona.kannard.healthylife
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            val channel = NotificationChannel(R.string.CHANNEL_ID.toString(), name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createNotificationChannel()
         //Button Water
         val waterButton = findViewById<Button>(R.id.water_activity_button)
         waterButton?.setOnClickListener()
