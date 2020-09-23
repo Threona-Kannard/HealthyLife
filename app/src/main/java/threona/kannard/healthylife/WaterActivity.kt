86ba71fdb39f4be54514f06ef8d527bc8d74b842
ZiiS-ND <nguyenkhang.ziis.nd@gmail.com>
2020-09-23 16:52:41 +0700

    Resolve Conflict

--- app/src/main/java/threona/kannard/healthylife/WaterActivity.kt
+++ app/src/main/java/threona/kannard/healthylife/WaterActivity.kt
@@ -1,11 +1,15 @@
 package threona.kannard.healthylife
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import java.io.*
 import java.time.LocalDate
 
@@ -13,7 +17,7 @@ class WaterActivity : AppCompatActivity() {
 
     val currentDate = LocalDate.now()
 
-    private var total: Float = 0f;
+    private var total: Float = 0f
     val Waterfilename: String= "info_water.txt"
     var Waterdata: String ="0.00"
     var dataLoad: String =""
@@ -23,6 +27,20 @@ class WaterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_water)
 
         val btn_water_icon = findViewById<Button>(R.id.add_water)
+
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(this, WaterActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+        var builder = NotificationCompat.Builder(this, R.string.CHANNEL_ID.toString())
+            .setSmallIcon(R.drawable.water_icon)
+            .setContentTitle("Water")
+            .setContentText("Drink Now !!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
         var file = File(filesDir.absolutePath, Waterfilename)
 
@@ -46,7 +64,7 @@ class WaterActivity : AppCompatActivity() {
         }
 
         //work with load data
-        val res = dataLoad.split(";");
+        val res = dataLoad.split(";")
 
         if(res.size == 2) {
 
@@ -59,7 +77,7 @@ class WaterActivity : AppCompatActivity() {
             }
             else {
                 total = res[0].toFloat()
-                findViewById<TextView>(R.id.Water_count).setText(total.toString())
+                findViewById<TextView>(R.id.Water_count).text = total.toString()
             }
         }
 
@@ -69,7 +87,8 @@ class WaterActivity : AppCompatActivity() {
             if (total<2){
                 val total_water = findViewById<TextView>(R.id.Water_count)
                 total = total+0.25f
-                total_water.setText(total.toString())}
+                total_water.text = total.toString()
+            }
 
             Waterdata = SaveString(total,currentDate.toString())
 
@@ -90,7 +109,18 @@ class WaterActivity : AppCompatActivity() {
             }
             Toast.makeText(applicationContext,Waterdata, Toast.LENGTH_LONG).show()
         }
+
+        val btn_5sec = findViewById<Button>(R.id.timer5sec)
+
+        btn_5sec?.setOnClickListener()
+        {
+            with(NotificationManagerCompat.from(this)) {
+                // notificationId is a unique int for each notification that you must define
+                notify(100, builder.build())
+            }
+        }
     }
+
 
     private fun SaveString(total: Float, dateInString: String): String {
         return total.toString() + ";" + dateInString
